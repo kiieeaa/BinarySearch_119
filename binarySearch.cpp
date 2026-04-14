8cc15796abfe166f0863e75a0dce5a8c992a9652
kiieeaa <kiyaadinda5@gmail.com>
2026-04-14 11:10:15 +0700

    pencarian binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,79 +1,104 @@
 #include <iostream>
 using namespace std;
 
+// Variabel Global
 int element[10];
 int nPanjang;
 int x;
 
-void input()
-{
-    while (true)
-    {
-        cout << "masukan banyaknya element pada array (maksimal 10): ";
+void input() {
+    while (true) {
+        cout << "Masukan banyaknya element pada array (maksimal 10): ";
         cin >> nPanjang;
 
-        if (nPanjang <= 10)
-        {
+        if (nPanjang <= 10 && nPanjang > 0) {
             break;
-        
-        }
-        else
-        {
-            cout << "\n[!] jumlah element tidak boleh lebih dari 10. silahkan coba lagi.\n\n";      
+        } else {
+            cout << "\n[!] Jumlah element harus antara 1-10. Silahkan coba lagi.\n\n";      
         }
     }
 
     cout << "\n==================\n";
-    cout << " masukan element array \n";
+    cout << " Masukan element array \n";
     cout << "==================\n";
 
-    for (int i = 0; i < nPanjang; i++)
-    {
-        cout << "element ke-" << i + 1 << ": ";
+    for (int i = 0; i < nPanjang; i++) {
+        cout << "Element ke-" << i + 1 << ": ";
         cin >> element[i];
     }
 }
 
-void bubbleSort()
-{
-    int pass = 1;
-    do
-    {
-        for (int j = 0; j <= nPanjang - 1 - pass; j++)
-        {
-            if (element[j] > element[j + 1])
-            {
+void bubbleSort() {
+    for (int i = 0; i < nPanjang - 1; i++) {
+        for (int j = 0; j < nPanjang - i - 1; j++) {
+            if (element[j] > element[j + 1]) {
                 int temp = element[j];
                 element[j] = element[j + 1];
                 element[j + 1] = temp;
             }
         }
-        pass++;
-    } while (pass <= nPanjang - 1 );
+    }
 }
 
-void display(){
+void display() {
     cout << "\n==================\n";
-    cout << " elemen array setelah diurutkan (Asc) \n";
+    cout << " Elemen array setelah diurutkan (Asc) \n";
     cout << "==================\n";
 
-    for (int j = 0; j < nPanjang; j++)
-    {
-        cout << element[j] ;
-        if (j < nPanjang - 1)
-        {
+    for (int j = 0; j < nPanjang; j++) {
+        cout << element[j];
+        if (j < nPanjang - 1) {
             cout << ", ";
         }
     }
     cout << endl;
-
 }
 
+void binarySearch() {
+    char ulang;
+    do {
+        cout << "\n===================\n";
+        cout << " PENCARIAN BINARY SEARCH \n";
+        cout << "===================\n";
 
-    
-
-
+        cout << "Masukan elemen yang ingin dicari: ";
+        cin >> x;
         
-            
-       
+        int low = 0;
+        int high = nPanjang - 1;
+        bool ditemukan = false;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (element[mid] == x) {
+                cout << "\n[v] Element " << x << " ditemukan pada indeks ke-" << mid << ".\n";
+                ditemukan = true;
+                break;
+            }
+
+            if (x < element[mid]) {
+                high = mid - 1; // Cari di bagian kiri
+            } else {
+                low = mid + 1;  // Cari di bagian kanan
+            }
+        }
+
+        if (!ditemukan) {
+            cout << "\n[x] Element " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+    } while (ulang == 'y' || ulang == 'Y');
+}
+
+int main() {
+    input();        // 1. Input data
+    bubbleSort();   // 2. Urutkan (Syarat mutlak Binary Search)
+    display();      // 3. Tampilkan hasil urutan
+    binarySearch(); // 4. Cari data
     
+    cout << "\nProgram Selesai. Terima kasih!\n";
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+pencarian binary search
